1152b2893efbbdcff6c90ff8df9fbed1ac9b083d
rjbelleza <rjbelleza19@gmail.com>
2025-04-13 02:36:12 +0800

    Update: User Model

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,7 +21,18 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'role'
     ];
+    
+    public function isAdmin(): bool
+    {
+        return $this->role === 'admin';
+    }
+    
+    public function isUser(): bool
+    {
+        return $this->role === 'user';
+    }
 
     /**
      * The attributes that should be hidden for serialization.
